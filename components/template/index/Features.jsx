35e6aef729dc3565d8afc2117c6c6e64eb4bec11
ali reza azdast <azdast47@gmail.com>
2025-04-10 11:55:23 +0330

    added pagination and catagory search it is my final submit it was intresting to work wiht page router of next it was not as hard as i expected but i rathere use app router

--- components/template/index/Features.jsx
+++ components/template/index/Features.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import {
+  faBarChart,
+  faGlobe,
+  faKey,
+  faLock,
+  faMapMarker,
+  faTrophy,
+} from "@fortawesome/free-solid-svg-icons";
+config.autoAddCss = false;
 
 function Features() {
   return (
     <div className="featurs">
       <div className="featur">
         <span className="featur__icon">
-          <i className="fa fa-globe"></i>
+          {/* <i className="fa fa-globe"></i> */}
+          <FontAwesomeIcon icon={faGlobe} />
         </span>
         <h4 className="u-heading--dark">بهترین خانه های لوکس جهان</h4>
         <p className="u-paragraph">
@@ -17,7 +30,8 @@ function Features() {
 
       <div className="featur">
         <span className="featur__icon">
-          <i className="fa fa-map-marker"></i>
+          {/* <i className="fa fa-map-marker"></i> */}
+          <FontAwesomeIcon icon={faMapMarker} />
         </span>
         <h4 className="u-heading--dark">همه خانه ها در مکان های برتر</h4>
         <p className="u-paragraph">
@@ -27,7 +41,8 @@ function Features() {
       </div>
       <div className="featur">
         <span className="featur__icon">
-          <i className="fa fa-key"></i>
+          {/* <i className="fa fa-key"></i> */}
+          <FontAwesomeIcon icon={faKey} />
         </span>
         <h4 className="u-heading--dark">خانه جدید در یک هفته</h4>
         <p className="u-paragraph">
@@ -38,7 +53,8 @@ function Features() {
       </div>
       <div className="featur">
         <span className="featur__icon">
-          <i className="fa fa-bar-chart-o"></i>
+          {/* <i className="fa fa-bar-chart-o"></i> */}
+          <FontAwesomeIcon icon={faBarChart} />
         </span>
         <h4 className="u-heading--dark">فقط بهترین خواص</h4>
         <p className="u-paragraph">
@@ -49,7 +65,8 @@ function Features() {
       </div>
       <div className="featur">
         <span className="featur__icon">
-          <i className="fa fa-lock"></i>
+          {/* <i className="fa fa-lock"></i> */}
+          <FontAwesomeIcon icon={faLock} />
         </span>
         <h4 className="u-heading--dark">پرداخت های ایمن در بعدی</h4>
         <p className="u-paragraph">
@@ -60,7 +77,8 @@ function Features() {
       </div>
       <div className="featur">
         <span className="featur__icon">
-          <i className="fa fa-trophy"></i>
+          {/* <i className="fa fa-trophy"></i> */}
+          <FontAwesomeIcon icon={faTrophy} />
         </span>
         <h4 className="u-heading--dark">1% مشاوران املاک برتر</h4>
         <p className="u-paragraph">
